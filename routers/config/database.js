f9be9a01a75717c2f4a3220f4bce78f49454a2df
leejinwoo810 <jinw810@gmail.com>
2022-02-17 20:26:59 +0900

    22.02.17 db

--- routers/config/database.js
+++ routers/config/database.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var db_info = {
     host: 'localhost',
     port: '3306',
-    user: 'user',
-    password: '0000',
+    user: 'root',
+    password: '',
     database: 'test_db'
 }
 
