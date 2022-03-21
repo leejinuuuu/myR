d517cc56516e1882e03216ca84b59fe6396f132f
leejinwoo810 <jinw810@gmail.com>
2022-03-21 14:27:28 +0900

    change name when already exist same name of image

--- routers/images.js
+++ routers/images.js
@@ -6,16 +6,34 @@ const path = require('path');
 
 var fs = require('fs');
 
+
+// var rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
+var rootPath = "C:/Users/jinw8/Desktop";
+var basePath = rootPath + "/" + "springboot_upload/";
+
+
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, __dirname + '/res/');
+        cb(null, basePath);
     },
   
     filename: function(req, file, cb) {
-        cb(null, file.originalname);
+        console.log("file is exist ? " + fs.existsSync(basePath+file.originalname))
+        var i = 1;
+        var fileName = file.originalname
+        console.log("file.originalname.lastIndexOf('.') : " + file.originalname.lastIndexOf('.'));
+
+        while(fs.existsSync(basePath+fileName)) {
+            fileName = file.originalname.slice(0, -4).concat("("+i+")").concat(file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length));
+            console.log("fileName : " + fileName);
+            i++;
+        }
+
+        cb(null, fileName);
     }
   });
   
+
 var upload = multer({ storage: storage })
 
 router.post(['/uploads'], upload.any(), (req, res) => {
@@ -25,7 +43,7 @@ router.post(['/uploads'], upload.any(), (req, res) => {
 router.get('/:name', function(req, res, next) {
     var params = req.params;
     if(params.name != null) {
-        fs.readFile(__dirname + '\\res\\' + params.name, function(err, data) {
+        fs.readFile(basePath + params.name, function(err, data) {
             res.writeHead(200);
             res.write(data);
             res.end();
